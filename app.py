32ce63b19b8408a7e05b1caf73eebcd7fb6afb3e
teodoro <teoraulino@gmail.com>
2023-11-12 17:36:47 -0300

    Show result

--- app.py
+++ app.py
@@ -9,24 +9,23 @@ def index():
 @app.route('/api/data', methods=['GET', 'POST'])
 def api_data():
     if request.method == 'GET':
-        # Lógica para manipular requisições GET
         return 'Endpoint GET'
 
     elif request.method == 'POST':
-        # Lógica para manipular requisições POST
-        data = request.json  # Supondo que os dados são enviados como JSON
+        data = request.json
         tipo = data.get('tipo')
         valor = data.get('valor')
 
         if tipo is not None and valor is not None:
-            # Aqui você pode realizar ações com base nos valores recebidos
-            # Por exemplo, imprimir o tipo e o valor no console
+
             print(f'Tipo: {tipo}, Valor: {valor}')
 
-            # Pode retornar uma resposta JSON indicando sucesso
-            return jsonify({'success': True, 'message': 'Dados recebidos com sucesso.'})
+            return jsonify({
+                'success': True, 
+                'message': 'Dados recebidos com sucesso.',
+                'object': data
+                })
         else:
-            # Se os parâmetros esperados não foram fornecidos, retornar um erro
             return jsonify({'error': 'Parâmetros ausentes ou inválidos.'}), 400
 
 if __name__ == '__main__':
